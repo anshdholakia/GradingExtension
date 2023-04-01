61871f364cfdbba9fa9f7354983608659aeeb1d9
Ansh Dholakia <anshhiro.dholakia@gmail.com>
2023-04-01 15:56:08 -0400

    transferring message between the content and manifest

--- popup/popup.js
+++ popup/popup.js
@@ -5,6 +5,17 @@ function popup() {
     });
 }
 
-document.getElementById("#flexSwitchCheckDefault").addEventListener("click", ()=>{
-    
-})
+document.addEventListener("DOMContentLoaded", function() {
+    // send a message to the content js to get the status of the grading
+    popup();
+});
+
+
+// getting back the message from the content js about the status of the grading
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "start" ) {
+            start();
+        }
+    }
+);
--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,27 @@
-window.addEventListener("locationchange", ()=>{
+function addGradingSection() {
     console.log("Hi");
-    if (document.getElementById("crumb_2") && document.getElementById("crumb_2").textContent.trim() === "Grade Center" && document.getElementById("breadcrumb_controls_id")) {
-        let element = document.createElement("div");
-        element.style.width = "250px";
-        element.style.height = "400px";
-        element.style.position = "absolute";
-        element.style.border = "2px solid black";
-        element.style.right = "20px";
-        element.style.bottom = "0px";
-        element.style.backgroundColor = "white";
-        element.style.zIndex = "10000";
-        document.body.appendChild(element);
+    let element = document.createElement("div");
+    element.style.width = "250px";
+    element.style.height = "400px";
+    element.style.position = "absolute";
+    element.style.border = "2px solid black";
+    element.style.right = "20px";
+    element.style.bottom = "0px";
+    element.style.backgroundColor = "white";
+    element.style.zIndex = "10000";
+    document.body.appendChild(element);
+}
 
+// adding a listener to the content to listen to messages from the popup js when it is opened
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message === "start") {
+            // sending a message to the popup.js to keep the switch unchecked
+            chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+                var activeTab = tabs[0];
+                chrome.tabs.sendMessage(activeTab.id, { "status": "off" });
+            });
+
+        }
     }
-
-})
-
+);
