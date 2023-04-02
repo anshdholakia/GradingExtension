2e9379750f01bc5fabd2a8a5e279be1a2b2d2e6d
Ansh Dholakia <anshhiro.dholakia@gmail.com>
2023-04-02 00:00:03 -0400

    using indexeddb to log info

--- scripts/content.js
+++ scripts/content.js
@@ -12,23 +12,31 @@ function addGradingSection() {
     document.body.appendChild(element);
 }
 
-let grade_status = false;
+const dbName = 'logs';
+const storeName = 'gradestate';
 
-// adding a listener to the content to listen to messages from the popup js when it is opened
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "start") {
-            // sending a message to the popup.js to keep the switch unchecked
-            chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-                var activeTab = tabs[0];
-                if (!grade_status) {
-                    chrome.tabs.sendMessage(activeTab.id, { "status": "off" });
-                }
-                else {
-                    chrome.tabs.sendMessage(activeTab.id, { "status": "on" });
-                }
-            });
+const openDB = () => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName, 1);
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains(storeName)) {
+        db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+      }
+    };
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+};
 
-        }
-    }
-);
+const addItem = async (item) => {
+  const db = await openDB();
+  const tx = db.transaction(storeName, 'readwrite');
+  const store = tx.objectStore(storeName);
+  const request = store.add(item);
+  request.onsuccess = () => console.log('Item added');
+  request.onerror = () => console.error('Error adding item');
+};
+
+// Add an item to the IndexedDB
+addItem({ "state": false });
